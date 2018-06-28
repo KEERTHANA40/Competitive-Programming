4454b9d8d0f207da7be51d0caba610ec3e0a02e5
KEERTHANA40 <31198600+KEERTHANA40@users.noreply.github.com>
2018-06-28 16:40:04 +0530

    Add files via upload

---
+++ Week 1/Day 4/ordered_set.py
@@ -0,0 +1,87 @@
+import unittest
+
+def binarysearch(arr,l,h,num):
+    while l<=h:
+        n=(h+l)//2
+        if num>arr[n]:
+            l=n+1
+        elif num<arr[n]:
+            h=n-1
+        else:
+            return True
+    return False
+
+
+def contains(ordered_list, number):
+
+    # Check if an integer is present in the list
+    
+
+    # return False
+    n=len(ordered_list)
+    output=binarysearch(ordered_list,0,n-1,number)
+    return output
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_empty_list(self):
+        result = contains([], 1)
+        self.assertFalse(result)
+
+    def test_one_item_list_number_present(self):
+        result = contains([1], 1)
+        self.assertTrue(result)
+
+    def test_one_item_list_number_absent(self):
+        result = contains([1], 2)
+        self.assertFalse(result)
+
+    def test_small_list_number_present(self):
+        result = contains([2, 4, 6], 4)
+        self.assertTrue(result)
+
+    def test_small_list_number_absent(self):
+        result = contains([2, 4, 6], 5)
+        self.assertFalse(result)
+
+    def test_large_list_number_present(self):
+        result = contains([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 8)
+        self.assertTrue(result)
+
+    def test_large_list_number_absent(self):
+        result = contains([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0)
+        self.assertFalse(result)
+
+    def test_large_list_number_first(self):
+        result = contains([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1)
+        self.assertTrue(result)
+
+    def test_large_list_number_last(self):
+        result = contains([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10)
+        self.assertTrue(result)
+
+
+unittest.main(verbosity=2)
