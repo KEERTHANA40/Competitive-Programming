6ef70fa7be32c84e235fb4318244a372645b3a49
KEERTHANA40 <31198600+KEERTHANA40@users.noreply.github.com>
2018-07-09 10:26:14 +0530

    Add files via upload

---
+++ Week 2/day4/permutations_palindrome.py
@@ -0,0 +1,48 @@
+import unittest
+
+def has_palindrome_permutation(string):
+
+    # Check if any permutation of the input is a palindrome
+    dictionary = {}
+    for i in string:
+        if i in dictionary:
+            dictionary[i]+=1
+        else:
+            dictionary[i]=1
+    
+    count = 0
+    for i in dictionary:
+        if dictionary[i]%2==1:
+            count+=1
+
+    return count==1 or count==0
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_permutation_with_odd_number_of_chars(self):
+        result = has_palindrome_permutation('aabcbcd')
+        self.assertTrue(result)
+
+    def test_permutation_with_even_number_of_chars(self):
+        result = has_palindrome_permutation('aabccbdd')
+        self.assertTrue(result)
+
+    def test_no_permutation_with_odd_number_of_chars(self):
+        result = has_palindrome_permutation('aabcd')
+        self.assertFalse(result)
+
+    def test_no_permutation_with_even_number_of_chars(self):
+        result = has_palindrome_permutation('aabbcd')
+        self.assertFalse(result)
+
+    def test_empty_string(self):
+        result = has_palindrome_permutation('')
+        self.assertTrue(result)
+
+    def test_one_character_string(self):
+        result = has_palindrome_permutation('a')
+        self.assertTrue(result)
+
+
+unittest.main(verbosity=2)
